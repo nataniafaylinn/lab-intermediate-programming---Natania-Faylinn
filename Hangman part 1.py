74e909b5f18d59168dd818d6a553d92e16e4d3fe
natania faylinn anggunjaya <natania.faylinn@gmail.com>
2024-03-13 10:57:28 +0700

    push tugas lab 2

---
+++ Hangman part 1.py
@@ -0,0 +1,29 @@
+def main ():
+    secret = "help"
+    guess = "_" *len(secret)
+    correct = []
+    print ("The word have",len(secret),"letters")
+
+    while True:
+        print ("Guess this",guess)
+        letter = input("Guess the letter !").lower()
+        if letter in secret:
+            correct.append(letter)
+            newguess = ""
+            for i in range(len(secret)):
+                if secret[i] == letter:
+                    newguess += letter
+                else:
+                    newguess += guess[i]
+            guess = newguess
+            print("You have guessed the letter")
+            if "_" not in guess:
+                print("You have guessed the word =",secret)
+                break
+        elif not letter.isalpha():
+            print("Please input a letter")
+        else:
+            print("You have not guessed the letter")
+
+main()
+    
